291042009ce2ede81411865e15ba933385d13c95
Arfan232353 <arfanbhuiyanshawon@gmail.com>
2025-02-13 01:29:19 +0000

    Conversion of Data types

---
+++ .devcontainer/01_basics/03_conversionOperations.js
@@ -0,0 +1,13 @@
+let score = "arfan"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score);
+
+console.log(typeof score);
+
+console.log(valueInNumber);
+
+let isLoggedIn = 1
+
