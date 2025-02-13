75eb60be88dc4e87987a741eaabd8ccc0fac08d6
Arfan232353 <arfanbhuiyanshawon@gmail.com>
2025-02-13 00:30:16 +0000

    xyz

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,21 @@
+const accountId = 144553
+let accountEmail = "arfan@google.com"
+var accountPassword = "12345"
+accountCity = "RAK"
+
+// accountId = 2 not allowed
+
+accountEmail = "shawon.com"
+accountPassword = "45678"
+accountCity = "Abo Dhabi"
+
+let accountState;
+
+console.log(accountId);
+
+/*
+not use var
+
+*/
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
