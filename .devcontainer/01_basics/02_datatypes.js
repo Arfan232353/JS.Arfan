90a1d7011e2a72a8a0e198433183e752559d83c8
Arfan232353 <arfanbhuiyanshawon@gmail.com>
2025-02-13 01:11:21 +0000

    Datatypes

---
+++ .devcontainer/01_basics/02_datatypes.js
@@ -0,0 +1,11 @@
+ "use strict"; //treat all JS code as newer version
+
+ //alert("3 + 3") we are using nodejs, not browser
+
+console.log("Arfan");
+
+let name = "Arfan"
+let age = 18
+let isLoggedIn = true
+
+console.log(typeof undefined);
