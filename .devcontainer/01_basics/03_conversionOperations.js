dcb03e4eecabf4ae8828283ec201d12411e9ce7f
Arfan232353 <arfanbhuiyanshawon@gmail.com>
2025-02-16 01:02:01 +0000

    ConversionOperations

--- .devcontainer/01_basics/03_conversionOperations.js
+++ .devcontainer/01_basics/03_conversionOperations.js
@@ -1,13 +1,28 @@
 let score = "arfan"
 
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
-let valueInNumber = Number(score);
+// let valueInNumber = Number(score);
 
-console.log(typeof score);
+// console.log(typeof score);
 
-console.log(valueInNumber);
+// console.log(valueInNumber);
 
-let isLoggedIn = 1
+// let isLoggedIn = 1
+
+let str1 = "hello"
+
+let srt2 = " Arfan"
+
+let srt3 = str1 + srt2
+
+// console.log(srt3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(3 + 4 * 5 % 3);''
 
