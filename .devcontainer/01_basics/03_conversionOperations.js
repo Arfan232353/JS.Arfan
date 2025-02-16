70b5a4670a2db5c70dd22c50f0e90590f987271f
Arfan232353 <arfanbhuiyanshawon@gmail.com>
2025-02-16 01:53:31 +0000

    ConversionOperations

--- .devcontainer/01_basics/03_conversionOperations.js
+++ .devcontainer/01_basics/03_conversionOperations.js
@@ -26,3 +26,18 @@ let srt3 = str1 + srt2
 
 // console.log(3 + 4 * 5 % 3);''
 
+// console.log(+"");
+
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2+2
+
+// console.log(num1);
+// console.log(num2);
+// console.log(num3);
+
+let gameCounter = 100
+
+++gameCounter;
+console.log(gameCounter);
