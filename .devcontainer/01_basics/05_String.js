dcf4ae87ae897e08ee118f3f58d922cbdffde174
Arfan232353 <arfanbhuiyanshawon@gmail.com>
2025-02-16 04:11:02 +0000

    String Method

---
+++ .devcontainer/01_basics/05_String.js
@@ -0,0 +1,20 @@
+const name = "Arfan"
+const repoCount = 50
+
+console.log(name + repoCount + " Value");
+
+console.log(`Hello may name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String ('Arfan-Shawon')
+console.log(gameName);
+
+console.log(gameName.toUpperCase());
+console.log(gameName.toLowerCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('n'));
+
+const newString = gameName.substring(0, 3)
+console.log(newString);
+
+const anotherString = gameName.slice(-7, 4)
+console.log(anotherString);
