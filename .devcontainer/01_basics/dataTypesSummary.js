19dc6639efb64b276e903cea5c2abb497d969f25
Arfan232353 <arfanbhuiyanshawon@gmail.com>
2025-02-16 03:36:33 +0000

    Stack and Heap mamory

--- .devcontainer/01_basics/dataTypesSummary.js
+++ .devcontainer/01_basics/dataTypesSummary.js
@@ -1,15 +1,44 @@
 //Array, Object, Function
 
-const heros = ["john wick", "wingston", "Poo"]
+// const heros = ["john wick", "wingston", "Poo"]
 
-let myObj = {
-    name: "Arfan",
-    age: 22,
-}
+// let myObj = {
+//     name: "Arfan",
+//     age: 22,
+// }
 
-const myFunction = function () {
-    console.log("hello world");
+// const myFunction = function () 
+//     console.log("hello world");
     
+// }
+
+// console.log(typeof myFunction);
+
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+//Stack (Primitive), Heap (Non- Primitive)
+
+
+let myFullName = "Arfan Bhuiyan"
+
+let anothrName = myFullName
+
+
+
+anothrName = "Shawon"
+console.log(myFullName);
+console.log(anothrName);
+
+let userOne = {
+    email: "user@google.com",
+    upi: "user@bl"
 }
 
-console.log(typeof myFunction);
+let userTwo = userOne
+
+userTwo.email = "arfan@google.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
+
