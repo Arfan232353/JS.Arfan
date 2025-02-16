7d47d6c836941185066fde42829dd917c6fe2137
Arfan232353 <arfanbhuiyanshawon@gmail.com>
2025-02-16 03:15:41 +0000

    datatype summary

---
+++ .devcontainer/01_basics/dataTypesSummary.js
@@ -0,0 +1,15 @@
+//Array, Object, Function
+
+const heros = ["john wick", "wingston", "Poo"]
+
+let myObj = {
+    name: "Arfan",
+    age: 22,
+}
+
+const myFunction = function () {
+    console.log("hello world");
+    
+}
+
+console.log(typeof myFunction);
