9bfe98ff7544baa63d06c6aa693d3cc10f0502c5
Arfan232353 <arfanbhuiyanshawon@gmail.com>
2025-02-18 02:04:56 +0000

    Dates

---
+++ .devcontainer/01_basics/07_datesJS.js
@@ -0,0 +1,23 @@
+// Dates
+
+let myDate = new Date()
+// console.log(myDate.toString());
+
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toLocaleTimeString());
+// console.log(myDate.toTimeString());
+// console.log(myDate.toUTCString());
+// console.log(typeof myDate);
+
+// let myCreatDate = new Date(1994, 4, 10)
+// let myCreatDate = new Date(1994, 4, 10 , 5, 3)
+let myCreatDate = new Date()
+console.log(myCreatDate.toLocaleString());
+
+
+
+
