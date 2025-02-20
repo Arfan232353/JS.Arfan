d82fb1422864b46dbde5d95b345c586286d13b69
Arfan232353 <arfanbhuiyanshawon@gmail.com>
2025-02-20 00:35:55 +0000

    Array methods

--- .devcontainer/02_basics/02_arrays.js
+++ .devcontainer/02_basics/02_arrays.js
@@ -1,1 +1,25 @@
-const marvelHeros
+const marvel_heros = ["thor", "Ironman", "Spaiderman"]
+const dc = ["Supperman", "flash", "batman"]
+
+// marvel_heros.push(dc)
+// console.log(marvel_heros);
+
+// const allHeros = marvel_heros.concat(dc)
+// console.log(marvel_heros);
+// console.log(allHeros);
+
+
+const all_new_heros = [...marvel_heros, ...dc]
+
+// console.log(all_new_heros);
+
+const another_arry = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
+
+const real_another_array = another_arry.flat(Infinity)
+
+console.log(another_arry);
+console.log(real_another_array);
+
+console.log(Array.isArray("Arfan"))
+console.log(Array.from("Arfan"))
+
